ac777f611b6406396acee542964b8105de2334c0
Romuga <GRomuga@cgiar.org>
2018-02-12 14:33:31 +0800

    Update view for adapter

--- app/src/main/java/generomuga/com/crud/CustomAdapter.java
+++ app/src/main/java/generomuga/com/crud/CustomAdapter.java
@@ -14,6 +14,9 @@ import java.util.ArrayList;
 
 public class CustomAdapter extends ArrayAdapter<Message>{
 
+    Context mContext;
+    ArrayList<Message> dataSet;
+
     private static class ViewHolder{
         TextView mMessage;
         Button mDelete;
@@ -22,6 +25,8 @@ public class CustomAdapter extends ArrayAdapter<Message>{
 
     public CustomAdapter(ArrayList<Message> data, Context context) {
         super(context, R.layout.list_view, data);
+        this.dataSet = data;
+        this.mContext = context;
     }
 
     @NonNull
@@ -29,16 +34,24 @@ public class CustomAdapter extends ArrayAdapter<Message>{
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
 
         Message message1 = getItem(position);
-        ViewHolder viewHolder;
+        final ViewHolder viewHolder;
 
         if (convertView == null){
             viewHolder = new ViewHolder();
             LayoutInflater inflater = LayoutInflater.from(getContext());
             convertView = inflater.inflate(R.layout.list_view, parent, false);
 
+            viewHolder.mMessage = (TextView) convertView.findViewById(R.id.textViewMessage);
+            viewHolder.mDelete = (Button) convertView.findViewById(R.id.buttonDelete);
+            viewHolder.mUpdate = (Button) convertView.findViewById(R.id.buttonUpdate);
+
             convertView.setTag(viewHolder);
         }
+        else{
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
 
+        viewHolder.mMessage.setText(message1.getMessage());
 
         return convertView;
     }
