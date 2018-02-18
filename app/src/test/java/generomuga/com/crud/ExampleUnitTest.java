ec5db6238b184d1fcd6ccafc86c0f37a1636e6dd
Gene Romuga <generomuga12693@gmail.com>
2018-02-18 14:17:26 +0800

    Add test

--- app/src/test/java/generomuga/com/crud/ExampleUnitTest.java
+++ app/src/test/java/generomuga/com/crud/ExampleUnitTest.java
@@ -1,5 +1,6 @@
 package generomuga.com.crud;
 
+import org.junit.Rule;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -10,6 +11,10 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+
+    @Rule
+
+
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
